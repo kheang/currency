a4f51aefde35b18e00fa2d58dec3148bd8463628
Kheang Lim <kheang@gmail.com>
2014-09-24 14:57:50 -0400

    Added test: Add two currency objects with same code

--- currency_proj/lib/currency.rb
+++ currency_proj/lib/currency.rb
@@ -1,9 +1,22 @@
 class Currency
+  include Comparable
   attr_reader :num, :code
 
-  def initialize(num = 0,curr_code)
+  def initialize(num,curr_code)
     @num = num
     @code = curr_code
+  end
+
+  def <=>(comparison)
+    if @code == comparison.code
+      @num <=> comparison.num
+    end
+  end
+
+  def +(comparison)
+    if @code == comparison.code
+      currency_sum = Currency.new(@num+comparison.num,@code)
+    end
   end
 
   def run
--- currency_proj/test/currency_test.rb
+++ currency_proj/test/currency_test.rb
@@ -12,12 +12,20 @@ class CurrencyProjTest < MiniTest::Unit::TestCase
     assert_equal "USD", currency.code
   end
 
-  def test_two_objects_equal_each_other
-    currency1 = Currency.new(1,"USD")
-    currency2 = Currency.new(1,"USD")
+  def test_two_currency_objects_equal_each_other
+    currency1 = Currency.new(3,"USD")
+    currency2 = Currency.new(3,"USD")
 
     assert_equal currency1.num, currency2.num
     assert_equal currency1.code, currency2.code
+  end
+
+  def test_add_two_currency_objects_with_same_code
+    currency1 = Currency.new(4,"USD")
+    currency2 = Currency.new(8,"USD")
+    currency_test_sum = currency1 + currency2
+
+    assert_equal Currency.new(12,"USD"), currency_test_sum
   end
 
 end
