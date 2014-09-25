7f8208f7e2e723fff23dc31dc3be32e7a4265acf
Kheang Lim <kheang@gmail.com>
2014-09-24 22:00:02 -0400

    Added tests: Converter accepts hash of values, can convert into any known currency, can raise error

--- currency_proj/test/currency_converter_test.rb
+++ currency_proj/test/currency_converter_test.rb
@@ -23,7 +23,45 @@ class CurrencyConverterTest < MiniTest::Unit::TestCase
     currency = Currency.new(25,:USD)
     converted_currency = curr_converter.convert(currency,:EUR)
 
-    assert_equal Currency.new(18.5,:EUR), converted_currency
+    assert_equal Currency.new(25*0.74,:EUR), converted_currency
   end
 
+  def test_create_curr_converter_with_hash_of_at_least_three_values
+    curr_codes_hash = {:USD=>1.0,:EUR=>0.74,:GBP=>0.61}
+    curr_converter = CurrencyConverter.new(curr_codes_hash)
+
+    assert_equal curr_codes_hash, curr_converter.codes
+  end
+
+  def test_can_return_converted_currencies_for_all_codes
+    curr_codes_hash = {:USD=>1.0,:EUR=>0.74,:GBP=>0.61}
+    curr_converter = CurrencyConverter.new(curr_codes_hash)
+
+    converted_curr_GBP_EUR = curr_converter.convert(Currency.new(50,:GBP),:EUR)
+    converted_curr_USD_GBP = curr_converter.convert(Currency.new(25,:USD),:GBP)
+
+    assert_equal Currency.new((50*0.74/0.61),:EUR),converted_curr_GBP_EUR
+    assert_equal Currency.new((25*0.61/1.00),:GBP),converted_curr_USD_GBP
+  end
+
+  def test_error_if_unknown_currency_code
+    curr_codes_hash = {:USD=>1.0,:EUR=>0.74,:GBP=>0.61}
+    curr_converter = CurrencyConverter.new(curr_codes_hash)
+
+    begin
+      converted_curr_GBP_CAD = curr_converter.convert(Currency.new(50,:GBP),:CAD)
+    rescue UnknownCurrencyCodeError => ex
+      result = :exception_unknown_code_handled
+    end
+
+    assert_equal :exception_unknown_code_handled, result
+
+    begin
+      converted_curr_AUD_USD = curr_converter.convert(Currency.new(50,:AUD),:USD)
+    rescue UnknownCurrencyCodeError => ex
+      result = :exception_unknown_code_handled2
+    end
+
+    assert_equal :exception_unknown_code_handled2, result
+  end
 end
